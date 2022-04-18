c5f594877848a1ca428221be4e33c8cf94248b8e
Sanghun Oh <otter.oh@gmail.com>
2022-04-18 21:42:56 +0900

    helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,10 @@
+// $ node -v
+// $ npm -v
+// $ npm i random   // install random module in vs code
+console.log('Hello World');
+// $ node helloworld.js
+const random = require('random');
+console.log(random);
+// $ node helloworld.js
+console.log(random.int(0, 100));
+// $ node helloworld.js
