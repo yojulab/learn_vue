0eab9a7921485eb0f75b25626f725a315319dd8f
Sanghun Oh <otter.oh@gmail.com>
2022-05-04 00:17:02 +0900

    add components

---
+++ src/App_components01.vue
@@ -0,0 +1,45 @@
+<template>
+  <div><BowToUser /> default value</div>
+  <div><BowToUser :message="message" /></div>
+  <button @click="changeMsg()">Change Msg</button>
+  <div><BowToUser message="Good Moning!" /></div>
+  <div><BowToUser /></div>
+  <h2>Hello World</h2>
+</template>
+
+<script>
+import BowToUser from "./components/BowToUser.vue";
+export default {
+  name: "App",
+  components: {
+    BowToUser: BowToUser,
+  },
+  data() {
+    return {
+      message: "Main App.vue !",
+      flag: true,
+    };
+  },
+  methods: {
+    changeMsg() {
+      if (this.flag) {
+        this.message = "Good Night!";
+      } else {
+        this.message = "Good Evening!";
+      }
+      this.flag = !this.flag;
+    },
+  },
+};
+</script>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BowToUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <h3>{{ msg }} - message : {{ message }}</h3>
+</template>
+<script>
+export default {
+  name: "BowToUser",
+  props: {
+    // message: String,
+    message: {
+      type: String,
+      default: "Hello World!",
+    },
+  },
+  data() {
+    return {
+      msg: "Bow to User",
+    };
+  },
+};
+</script>
+<style scoped></style>
