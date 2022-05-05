403fdb283d2220ca0161658381eb661c2f57ea96
Sanghun Oh <otter.oh@gmail.com>
2022-05-05 23:43:57 +0900

    add componets cleave

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <ProductList :products="products"></ProductList>
-  <div>inner data : {{ products }}</div>
+  <h2>Hello World</h2>
 </template>
 
 <script>
-import ProductList from "./components/ProductList.vue";
 export default {
   name: "App",
-  data() {
-    return {
-      message: "Hello Vue!",
-      products: [
-        { name: "Phone", price: 200, quantity: 3, company: "Apple", id: 1 },
-        { name: "Tablet", price: 300, quantity: 5, company: "Samsung", id: 2 },
-        { name: "Laptop", price: 400, quantity: 2, company: "Dell", id: 3 },
-        { name: "TV", price: 500, quantity: 7, company: "Sony", id: 4 },
-        { name: "Camera", price: 600, quantity: 4, company: "Nikon", id: 5 },
-      ],
-    };
-  },
-  components: {
-    ProductList,
-  },
+  // components: {
+  //   HelloWorld
+  // }
 };
 </script>
 
