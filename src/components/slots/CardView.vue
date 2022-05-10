e9c7a6dc532cf8248b01d1219e45798735f7f7ae
Sanghun Oh <otter.oh@gmail.com>
2022-05-10 22:48:42 +0900

    add slot

---
+++ src/components/slots/CardView.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <h3 class="card">Card Template</h3> -->
+  <div class="card">
+    <slot>default text</slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.card {
+  background: #9ec1e4;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 20px;
+}
+</style>
---
+++ src/components/slots/CardViewSlot.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <slot name="header">Header</slot>
+    </div>
+    <div class="card-body">
+      <slot name="body">Body</slot>
+    </div>
+    <div class="card-footer">
+      <slot name="footer">Footer</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.card {
+  background: #9ec1e4;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 20px;
+}
+.card-header {
+  margin: 2px;
+  background: #c6f2c9;
+}
+.card-body {
+  background: #c6f2c9;
+  margin: 2px;
+}
+.card-footer {
+  background: #c6f2c9;
+  margin: 2px;
+}
+</style>
