21060554a78241de862bdf8b2d1d6a25af1c36ef
Sanghun Oh <otter.oh@gmail.com>
2022-05-20 22:48:32 +0900

    move some files

---
+++ src/components/mixins/FeeAmount.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Fee Amount : {{ amount }}</div>
+  <button @click="addMoney(150)">(150)</button>
+  <button @click="addMoney(250)">(250)</button>
+  <button @click="addMoney(350)">(350)</button>
+  <button @click="addMoney(450)">(450)</button>
+</template>
+
+<script>
+export default {
+  name: "FeeAmount",
+  //   mixins: [FeeAmountMixin],
+  data() {
+    return {
+      amount: 0,
+    };
+  },
+  methods: {
+    addMoney(money) {
+      this.amount += money;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/mixins/SavingAmount.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Saving Amount : {{ amount }}</div>
+  <button @click="addMoney(1000)">(1000)</button>
+  <button @click="addMoney(2000)">(2000)</button>
+  <button @click="addMoney(3000)">(3000)</button>
+  <button @click="addMoney(4000)">(4000)</button>
+</template>
+
+<script>
+export default {
+  name: "SavingAmount",
+  //   mixins: [SavingAmountMixin],
+  data() {
+    return {
+      amount: 0,
+    };
+  },
+  methods: {
+    addMoney(money) {
+      this.amount += money;
+    },
+  },
+};
+</script>
+
+<style></style>
