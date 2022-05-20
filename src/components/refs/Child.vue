709c2cafc23e7f2eff2e7f26966932724bc91b01
Sanghun Oh <otter.oh@gmail.com>
2022-05-20 22:26:21 +0900

    add refs

---
+++ src/components/refs/Child.vue
@@ -0,0 +1,25 @@
+<template>
+  <h3>Child Componet</h3>
+  <input type="text" v-model="message" />
+</template>
+
+<script>
+export default {
+  name: "Child",
+  data() {
+    return {
+      message: "Hello World",
+    };
+  },
+  methods: {
+    sayMessage(message = "this is Child") {
+      if (message) {
+        this.message = message;
+      }
+      console.log(message);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/refs/Parent.vue
@@ -0,0 +1,38 @@
+<template>
+  <h3>Parent Componet</h3>
+  <input type="text" v-model="message" ref="inputRef" />
+  <button @click="inputRefMethod">input Ref</button>
+  <hr />
+  <Child v-if="showChild" ref="childRef" />
+  <button @click="showChild = !showChild">show Child</button>
+</template>
+
+<script>
+import Child from "./Child.vue";
+export default {
+  name: "Parent",
+  data() {
+    return {
+      showChild: true,
+      message: "Parent Message",
+    };
+  },
+  methods: {
+    inputRefMethod() {
+      // console.log(this.$refs.inputRef, this);
+      this.$refs.inputRef.value = "";
+      this.$refs.inputRef.focus();
+      this.$refs.inputRef.style.backgroundColor = "yellow";
+      console.log(this.$refs.inputRef, "inputRef");
+
+      this.$refs.childRef.sayMessage("This is Parent!");
+      console.log(this.$refs.childRef, "childRef");
+    },
+  },
+  components: {
+    Child,
+  },
+};
+</script>
+
+<style></style>
