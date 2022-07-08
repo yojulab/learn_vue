32f1088078792a2f6e1ed7c33a193cf1db9945eb
SanghunOh <otter.oh@gmail.com>
2022-07-08 12:54:24 +0900

    change some file

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,8 @@
 // $ node -v
 // $ npm -v
-// $ npm i random   // install random module in vs code
 console.log('Hello World');
 // $ node helloworld.js
+// $ npm i random   // install random module in vs code
 const random = require('random');
 console.log(random);
 // $ node helloworld.js
