a76bc41b2efae57939e5d5a22263bd4a2cdda48e
Sanghun Oh <otter.oh@gmail.com>
2022-09-25 21:35:14 +0900

    build

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import router from "./routers/router.js";
-import stores from "./stores/sharedStates.js";
+
+// import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const app = createApp(App);
-// app.use(router);
+
+import router from "./routers/router.js";
+app.use(router);
+
+import stores from "./stores/sharedStates.js";
 app.provide("stores", stores);
+
 app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
+export default defineConfig(({ command, mode, ssrBuild }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [vue()],
-    // base: process.env.VITE_BASE_PATH, // for build
-    base: "/learn_vue", // for build
-    build: {
-      outDir: "docs",
-      emptyOutDir: true,
-    },
-    resolve: {
-      alias: {
-        "/@composables": path.resolve(__dirname, "./src/composables"),
+  if (command === "dev") {
+    return defineConfig({
+      plugins: [vue()],
+      base: "/", // for build
+      // base: process.env.VITE_BASE_PATH, // for build
+    });
+  } else {
+    return defineConfig({
+      plugins: [vue()],
+      base: "/learn_vue/", // for build
+      build: {
+        outDir: "docs",
+        emptyOutDir: true,
       },
-    },
-  });
-};
+    });
+  }
+});
