c490fdde38e888865b009809d7c754dbcb63d32d
Sanghun Oh <otter.oh@gmail.com>
2024-09-22 04:58:55 +0000

    fix : upgrade version

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,16 @@
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  if (command === "dev") {
-    return defineConfig({
-      plugins: [vue()],
-      base: "/", // for build
-      // base: process.env.VITE_BASE_PATH, // for build
-    });
-  } else {
-    return defineConfig({
-      plugins: [vue()],
-      base: "/learn_vue/", // for build
-      build: {
-        outDir: "docs",
-        emptyOutDir: true,
-      },
-    });
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
-});
+})
